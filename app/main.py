968fc3421e0a6af6819ddab925072c92f5764626
igor-medvedkov <medvedkovigor@gmail.com>
2020-05-25 16:16:59 +0400

    Added proxy call

--- app/main.py
+++ app/main.py
@@ -1,8 +1,25 @@
 from flask import Flask
+from flask import request
+import requests
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/test')
 def hello_world():
     return 'Hello, World!'
 
+@app.route('/')
+def index():
+    return '<h1>Telegate bot proxy</h1>'
+
+@app.route('/msg')
+def msg():
+    botId = request.args.get('botid')
+    chatId = request.args.get('chatid')
+    messageText = request.args.get('message')
+
+    url = "https://api.telegram.org/bot%s/sendMessage?chat_id=%s&text=%s" % (botId, chatId, messageText)
+
+    response = requests.get(url)
+
+    return response.text
